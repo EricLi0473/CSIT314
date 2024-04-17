a4912dd864acadc9f003cfd7542a3b763a22c093
Eric Li <42987545+Youtongg4us@users.noreply.github.com>
2024-04-17 10:28:34 +0800

    Delete entity directory

--- entity/Admin.py
+++
@@ -1,7 +0,0 @@
-from entity.User import User
-
-class Admin(User):
-    def __init__(self,username = 'username',password = 'password',email = 'email',userStatus = 'valid'):
-        super().__init__(username = username,password = password,email = email,userType='admin',userStatus = userStatus)
-
-
